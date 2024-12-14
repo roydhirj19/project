ecda2e323c0241e54a8e23cc932c4e0d60d1b0e5
roydhirj19 <dhirajyadav1604@gmail.com>
2024-12-14 12:17:04 +0530

    add eventlistener to the page

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 console.log("dhiraj")
+let para = document.querySelector("p")
+let btn = document.querySelector('button')
+btn.addEventListener('click',function(){
+    let randomNum = Math.floor(Math.random()* 100)  + 1;
+    para.innerHTML = randomNum    
+})
